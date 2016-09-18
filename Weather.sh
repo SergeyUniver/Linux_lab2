7de801de5894b23604b7d0aa10b790d3918c87cd
DevNock <DevNock@gmail.com>
2016-09-18 23:34:52 +0300

    deletting index.html

--- Weather.sh
+++ Weather.sh
@@ -3,5 +3,6 @@ do
 	source ./config.ini
 	wget -N -q https://pogoda.yandex.by/minsk/
 	echo | grep -Po '<div class="current-weather__thermometer current-weather__thermometer_type_now">.[0-9]+' index.html | grep -Po '.[0-9]+'
+	rm index.html
 	sleep $sleeptime
 done
