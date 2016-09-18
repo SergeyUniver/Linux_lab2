d0839ee05b8863ae0214dc58c32bbbb8e2c02d01
DevNock <DevNock@gmail.com>
2016-09-18 23:16:51 +0300

    finish lab

---
+++ Weather.sh
@@ -0,0 +1,7 @@
+while :
+do
+	source ./config.ini
+	wget -N -q https://pogoda.yandex.by/minsk/
+	echo | grep -Po '<div class="current-weather__thermometer current-weather__thermometer_type_now">.[0-9]+' index.html | grep -Po '.[0-9]+'
+	sleep $sleeptime
+done
